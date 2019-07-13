4f036b57d87d75338cbe91004aa5bda1c6e4627c
cloudy-ninja <msr0327@outlook.com>
2019-07-13 12:37:36 -0700

    add constants and Home components

---
+++ components/Home/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+import constants from '../../constants';
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: constants.Colors.white,
+	},
+})
+
+export default styles
